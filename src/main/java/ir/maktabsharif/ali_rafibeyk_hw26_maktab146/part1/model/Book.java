a364bcb70c120e0a4c387c07a9fe391b21b54ce4
beykod <alirafibeyk@gmail.com>
2026-09-12 18:44:37 +0330

    feat: first structure

---
+++ src/main/java/ir/maktabsharif/ali_rafibeyk_hw26_maktab146/part1/model/Book.java
@@ -0,0 +1,56 @@
+package ir.maktabsharif.ali_rafibeyk_hw26_maktab146.part1.model;
+
+public class Book {
+    private Long id;
+    private String title;
+    private String author;
+    private String category;
+    private Double price;
+
+    public Book(String title, String author, String category, Double price) {
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
